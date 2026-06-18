3cecd5e7058a37788d77bf15d5825af896f7303d
jsavo223 <jaradsavo@gmail.com>
2026-06-18 10:22:30 -0800

    Update control_core.py

--- services/control_core.py
+++ services/control_core.py
@@ -1,12 +1,19 @@
 from services.plan_engine import create_simple_plan
+from services.executor import Executor
 
 
 class ControlCore:
+    def __init__(self):
+        self.executor = Executor()
+
     def create_mission(self, goal: str):
         plan = create_simple_plan(goal)
 
+        results = self.executor.execute_plan(plan)
+
         return {
             "goal": goal,
-            "status": "planned",
-            "plan": plan
+            "status": "completed",
+            "plan": plan,
+            "results": results
         }
